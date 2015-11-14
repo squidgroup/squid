baa607c8fe2eab69cd901443178b2c3999955618
Haycen <hassen.allegue@gmail.com>
2015-11-13 17:04:32 -0800

    Replace runSquid by runSQUID

---
+++ R/runSQUID.R
@@ -0,0 +1,11 @@
+#' Run SQUID app
+#'
+#' @return NULL
+#' @export
+runSQUID <- function() {
+  appDir <- system.file("shiny-squid", "myapp", package = "SQUID")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `squid`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
