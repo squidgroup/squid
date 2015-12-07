c071df3188c78fb43cad77d7cf568d88b8866937
Hassen Allegue <hassen.allegue@gmail.com>
2015-12-07 20:22:58 +0100

    test calling SQUID package functions from SQUID shiny app

--- R/runSQUIDfct.R
+++ R/runSQUIDfct.R
@@ -4,12 +4,6 @@
 #' @export
 runSQUIDfct <- function() {
   
-  appDir <- system.file("shiny-squid", "myapp", "include", "myInclude", "fullModel", "functions",
-                        package = "SQUID")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `squid`.", call. = FALSE)
-  }
-  
-  getwd()
+  print("test ***********")
    
 }
