f6d4db876e2ada67012680167bbf5b6a38f243d6
Haycen <hassen.allegue@gmail.com>
2016-03-29 16:31:09 -0700

    add runSQUIDfct() documentation

--- inst/shiny-squid/source/server/fullmodel/SVRFullModel.R
+++ inst/shiny-squid/source/server/fullmodel/SVRFullModel.R
@@ -218,7 +218,7 @@ SVRFullModel <- function(myModule, input, output, session){
          updateCheckboxInput(session, "isRunning", value = TRUE)
          
          # Call app main function
-         data <- SQUID::runSQUIDfct(input, myModule, TRUE) 
+         data <- SQUID::runSQUIDfct(input, myModule, plot=TRUE) 
          
          names(data$full_Data)    <- outputNames
          names(data$sampled_Data) <- outputNames
