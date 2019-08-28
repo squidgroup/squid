d43bd81701a968b6faebea104b0301f9eec69bfc
Hassen Allegue <h.all@disroot.org>
2019-08-28 16:45:06 -0400

    Launch squid in Rstudio browser

--- R/squidApp.R
+++ R/squidApp.R
@@ -1,15 +1,10 @@
 #' @export
 #' 
-squidApp <- function(launch.browser=TRUE, ...) {
-  
-  f   <- factor(c("a", "b"))
-  res <- lme4::dummy(f)
-  cat.var <- rep(1:2, 2)
-  res <- dim(arm::contr.bayes.unordered(cat.var))
+squidApp <- function(...) {
   
   appDir <- system.file("shiny-squid", package = "squid")
   
   if (appDir == "") stop("Could not find squid application directory. Try re-installing `squid` package.", call. = FALSE)
   
-  shiny::runApp(appDir=appDir, launch.browser=launch.browser, display.mode="normal", ...)
+  shiny::runApp(appDir=appDir, ...)
 }
