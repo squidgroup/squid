16d7c052a8f034868c8e5ef07ff4ef149dda6a1e
Joel Pick <joel.l.pick@gmail.com>
2021-10-13 16:53:11 +0100

    make structure updates

--- R/make_structure.R
+++ R/make_structure.R
@@ -8,92 +8,96 @@ is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
 }
 
 ## function to get sample size out of structure
-extract_N <- function(x) as.numeric(sub("\\w+\\((\\d+)\\)","\\1",x))
+extract_N <- function(x){
+ N <- as.numeric(sub("\\w+\\((\\d+)\\)","\\1",x))
+ # if(length(N)>1) for(i in 1:(length(N)-1)) if(!is.wholenumber(N[i+1]/N[i])) stop("Nested factors need to be completely balanced ", call.=FALSE) ##better error message needed
+ return(N)
+}
 
 ## function to get grouping level names out of structure
 extract_name <- function(x) sub("(\\w+)\\(\\d+\\)","\\1",x)
 
-## function to generate grouping levels
-generate_levels <- function(x, N, balanced=TRUE, randomise=TRUE){
 
-	Ns <- c(x,N)
-
-	levels <- matrix(NA,N,length(x))
-
-	if(balanced){
-		for(i in (length(x)):1){
-			levels[,i]<- rep(1:Ns[i],each=N/Ns[i])
-		}
-	}else{
-		## start with lowest level, sample all levels once then, sample with replacement to get uniform variation in sample
-		## do the same with the next level based on the number of the lower levels rather than the total sample size
-		## if samples completely randomly, some levels will have zero and so the number of levels won't be consistent with that specified. To avoid this we need to sample each one ones, and then sample all again with replacement
-		for(i in length(x):1){
-			if(i==length(x)){
-				levels[,i] <- sort(c(1:Ns[i],sample(1:Ns[i],N-Ns[i], replace=TRUE)))
-			}else{
-				a <- table(levels[,i+1])
-				b <- sort(c(1:Ns[i],sample(1:Ns[i],Ns[i+1]-Ns[i], replace=TRUE)))
-				levels[,i] <- do.call(c,lapply(1:Ns[i+1],function(j) rep(b[j],a[j])))
-			}
-		}
-	}
-
-	# randomise the order - this helps with creating crossed random effects
-	if(randomise) levels <- levels[sample(1:N), ]
+## generate levels with simple nested design
+# generate_levels <- function(x){
+# 	levels <- matrix(NA,x[length(x)],length(x))
+# 	for(i in (length(x)):1) levels[,i]<- rep(1:x[i],each=x[length(x)]/x[i])
+# 	return(levels)
+# } 
+generate_levels <- function(x){
+	levels <- matrix(NA,prod(x),length(x))
+	for(i in (length(x)):1) levels[,i]<- rep(1:prod(x[i:1]),each=prod(x)/prod(x[i:1]))
 	return(levels)
 } 
 
-## for tests for package - make a function to test whether its nested or not 
-## - any(colSums(table(x,y)>0)>0)
+cross_levels <-function(x){
+	crossed_lower_levels <- expand.grid(lapply(x,function(i) i[,ncol(i)])[length(x):1])[,length(x):1,drop=FALSE]    
+	# crossed_lower_levels <- expand.grid(lapply(x,function(i) i[,ncol(i)]))  
+    do.call(cbind,lapply( 1:ncol(crossed_lower_levels), function(j) x[[j]][crossed_lower_levels[,j],] ))
+}
+
+add_interactions <- function(all_levels, int){	
+	int_str <-do.call(cbind,lapply(strsplit(int, ":"), function(x){
+			z<- apply(all_levels[,x],2,function(y) {
+			  char_dif <- max(nchar(y)) - nchar(y)
+			  paste0(sapply(1:length(char_dif), function(i) paste0(rep(0,each=char_dif[i]),collapse="")),y)
+	    })
+			as.numeric(as.factor(apply(z, 1, paste, collapse="")))
+		}))
+	colnames(int_str)<-int
+	cbind(all_levels,int_str)
+
+}
 
 
-
-#' Make hierarchical structure
+#' Make nested and crossed balanced hierarchical structures
 #'
-#' @param structure A formula specifying the structure. See details.
-#' @param N Number of observations
-#' @param balanced Logical. Is the number of observations within each grouping level the same? Default TRUE. 
-#' @details structure ... 
+#' @param structure A formula specifying the structure and sample sizes at each level. See details.
+#' @param time_step Number of repeated observations at the lowest level
+#' @details Factors are input as a text string. The name of each factor is followed by the number of levels in that factor in brackets e.g. "individual(100)". Nested factors can be specified using "/", e.g. "population(2)/individual(2)", the lower levels being specified after the higher levels, and the sample sizes of the lower levels 
 #' @return 
 #' @examples
 #' 
 #' @export
-## balanced - is the number of observations within each grouping level the same?
-## - does not ensure that they are balanced across grouping levels
-## need to add in warnings if chose balanced and the numbers don't match
-make_structure <- function(structure, N, balanced=TRUE){
+
+
+make_structure <- function(structure, time_step=1){
 	## strip white space from structure
 	structure <- gsub("\\s","",structure)
 
 	## separate into crossed and nested components
 	components <- strsplit(structure, "\\+")[[1]]
+	comp2 <- 	components[!grepl(":",components)]
+	int <- 	components[grep(":",components)]
 
 	## split nested grouping levels
-	comp_list <- strsplit(components, "\\/")
+	comp_list <- strsplit(comp2, "\\/")
 
 	## separate names and sample sizes
 	comp_list_N <- lapply(comp_list,extract_N)
 	comp_names <- do.call(c,lapply(comp_list,extract_name))
 
 	## apply generate levels function to all components	
-	## randomise the order to make it crossed with the other random effects
-	all_levels <- do.call(cbind,lapply(comp_list_N,generate_levels,N=N, balanced=balanced, randomise=TRUE))
+	comp_levels <- lapply(comp_list_N,generate_levels)
+
+	## do crossing
+	all_levels <- cross_levels(comp_levels)
 
 	## give names to matrix
 	colnames(all_levels) <- comp_names
 
-	return(all_levels)
+	## add in interaction columns
+	all_levels_int<- if(length(int)>0){
+		add_interactions(all_levels, int)
+	}else{
+		all_levels
+	}
+
+	## repeat levels for number of time steps
+	repeat_levels <- all_levels_int[rep(1:nrow(all_levels_int),each=time_step),,drop=FALSE] 
+
+	return(repeat_levels)
 }
 
-
-
-
-##problems:
-## for example nest can be nested within mother and year, but mother and year are crossed - difficult to make a syntax that can create this, so I think we provide a simple function but also allow users to put in their own structure
-
-### a fundamental problem is that the data generation process, especially in terms of data structure, might not be the same as that which we use to model the data. But here we are simulating data according to a model. Not using the phenoptype in the data structure simulation will mean that we cant simulate some of the biological properties of different data structures.
-
-### two grouping levels that are nested within the same grouping level, but crossed with each other like nest of rearing and nest of origin are nested in year but crossed with each other
-# something like  sex[2] + year[20]/(nest[25] + individual[250])
-
+## potential error messages
+## - ) needs to be followed by +, / or end
